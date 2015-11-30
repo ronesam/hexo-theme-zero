a4a99e9d231b187f8e8129b6f804b873db4b58d0
ronesam <ronesam@qq.com>
2015-11-30 18:03:03 +0800

    更新全屏的代码。

--- source/static/js/hexo-theme-zero-aside.js
+++ source/static/js/hexo-theme-zero-aside.js
@@ -21,18 +21,174 @@ $(function() {
   //No.2 全屏/取消全屏
   var icon_full = $('.icon-fullscreen');
   icon_full.click(function(e) {
-    $('.wrapper').fullScreen({
-      'callback': function(fullscreen){
-        if(!fullscreen) {
-          icon_full.attr('title', '全屏').addClass('icon-fullscreen').removeClass('icon-screenshot');
-        } else {
-          icon_full.attr('title', '取消全屏').addClass('icon-screenshot').removeClass('icon-fullscreen');
-          //只有全屏化之后才会有.fullscreen，所以每次点击都要重新绑定
-          $('.fullscreen').scroll(function () {
-            showArrow($(this).scrollTop());
-          });
-        }
+    //全屏是个异步API，所以增加了状态改变后的回调
+    screenfull.toggle('', function(isFullscreen) {
+      if(!isFullscreen) {
+        icon_full.attr('title', '全屏').addClass('icon-fullscreen').removeClass('icon-screenshot');
+      } else {
+        icon_full.attr('title', '取消全屏').addClass('icon-screenshot').removeClass('icon-fullscreen');
       }
-    })
+    })    
+  });
+
+  //No.3 目录
+  var icon_summary = $('.icon-list-ol');
+  icon_summary.click(function() {
+
+  });
+
+  //No.4 评论
+  var icon_comments = $('.icon-comments');
+  icon_comments.click(function() {
+    
   })
-})
+});
+
+/*!
+* screenfull
+* v2.1.0 - 2015-11-30
+* v2.0.0 - 2014-12-22
+* (c) Sindre Sorhus; MIT License
+*/
+(function () {
+  'use strict';
+
+  var isCommonjs = typeof module !== 'undefined' && module.exports;
+  var keyboardAllowed = typeof Element !== 'undefined' && 'ALLOW_KEYBOARD_INPUT' in Element;
+
+  var fn = (function () {
+    var val;
+    var valLength;
+
+    var fnMap = [
+      [
+        'requestFullscreen',
+        'exitFullscreen',
+        'fullscreenElement',
+        'fullscreenEnabled',
+        'fullscreenchange',
+        'fullscreenerror'
+      ],
+      // new WebKit
+      [
+        'webkitRequestFullscreen',
+        'webkitExitFullscreen',
+        'webkitFullscreenElement',
+        'webkitFullscreenEnabled',
+        'webkitfullscreenchange',
+        'webkitfullscreenerror'
+
+      ],
+      // old WebKit (Safari 5.1)
+      [
+        'webkitRequestFullScreen',
+        'webkitCancelFullScreen',
+        'webkitCurrentFullScreenElement',
+        'webkitCancelFullScreen',
+        'webkitfullscreenchange',
+        'webkitfullscreenerror'
+
+      ],
+      [
+        'mozRequestFullScreen',
+        'mozCancelFullScreen',
+        'mozFullScreenElement',
+        'mozFullScreenEnabled',
+        'mozfullscreenchange',
+        'mozfullscreenerror'
+      ],
+      [
+        'msRequestFullscreen',
+        'msExitFullscreen',
+        'msFullscreenElement',
+        'msFullscreenEnabled',
+        'MSFullscreenChange',
+        'MSFullscreenError'
+      ]
+    ];
+
+    var i = 0;
+    var l = fnMap.length;
+    var ret = {};
+
+    for (; i < l; i++) {
+      val = fnMap[i];
+      if (val && val[1] in document) {
+        for (i = 0, valLength = val.length; i < valLength; i++) {
+          ret[fnMap[0][i]] = val[i];
+        }
+        return ret;
+      }
+    }
+
+    return false;
+  })();
+
+  var screenfull = {
+    request: function (elem) {
+      var request = fn.requestFullscreen;
+
+      elem = elem || document.documentElement;
+
+      // Work around Safari 5.1 bug: reports support for
+      // keyboard in fullscreen even though it doesn't.
+      // Browser sniffing, since the alternative with
+      // setTimeout is even worse.
+      if (/5\.1[\.\d]* Safari/.test(navigator.userAgent)) {
+        elem[request]();
+      } else {
+        elem[request](keyboardAllowed && Element.ALLOW_KEYBOARD_INPUT);
+      }
+    },
+    exit: function () {
+      document[fn.exitFullscreen]();
+    },
+    toggle: function (elem, fn) {
+      if (this.isFullscreen) {
+        this.exit();
+        fn(false);
+      } else {
+        this.request(elem);
+        fn(true);
+      }
+    },
+    raw: fn
+  };
+
+  if (!fn) {
+    if (isCommonjs) {
+      module.exports = false;
+    } else {
+      window.screenfull = false;
+    }
+
+    return;
+  }
+
+  Object.defineProperties(screenfull, {
+    isFullscreen: {
+      get: function () {
+        return !!document[fn.fullscreenElement];
+      }
+    },
+    element: {
+      enumerable: true,
+      get: function () {
+        return document[fn.fullscreenElement];
+      }
+    },
+    enabled: {
+      enumerable: true,
+      get: function () {
+        // Coerce to boolean in case of old WebKit
+        return !!document[fn.fullscreenEnabled];
+      }
+    }
+  });
+
+  if (isCommonjs) {
+    module.exports = screenfull;
+  } else {
+    window.screenfull = screenfull;
+  }
+})();
